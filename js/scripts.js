30b06b5c6809443d661722c9e1499cdae167e67e
cschreiner0 <75751364+cschreiner0@users.noreply.github.com>
2021-07-09 10:21:26 -0700

    Additions to all pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,28 @@
-//If everytihing is okay, console will say OK
-console.log("OK");
+// //If everytihing is okay, console will say OK
+// console.log("OK");
 
-//store background classes
-const backgrounds = ["bg0", "bg1", "bg2", "bg3"];
+// //store background classes
+// const backgrounds = ["bg0", "bg1", "bg2", "bg3"];
 
-//Get random numbers
-var numRand;
-numRand = Math.floor(Math.random() * backgrounds.length);
+// //Get random numbers
+// var numRand;
+// numRand = Math.floor(Math.random() * backgrounds.length);
 
-//Get element
-let pageBackgound = document.querySelector("body");
+// //Get element
+// let pageBackgound = document.querySelector("body");
 
-//Set class name from stored values
-pageBackgound.className = backgrounds[numRand];
+// //Set class name from stored values
+// pageBackgound.className = backgrounds[numRand];
 
-//Use Lightbox Gallery Library
-//lightGallery(document.getElementById("gallery"));
+// //Use Lightbox Gallery Library
+// //lightGallery(document.getElementById("gallery"));
 
-lightGallery(document.getElementById("captions"));
+// lightGallery(document.getElementById("captions"));
 
-const portfolio = document.querySelector("#portfolio");
-lightGallery(portfolio, {
-  selector: ".lglink",
-  download: false
-});
+// const portfolio = document.querySelector("#portfolio");
+// lightGallery(portfolio, {
+//   selector: ".lglink",
+//   download: false
+// });
+
+$(".carousel").carousel();
